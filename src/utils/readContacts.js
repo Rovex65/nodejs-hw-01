e4b43796752a8f375371f8240c1cb663957d9ce6
Rovex65 <rovex65@gmail.com>
2024-10-02 17:13:26 +0200

    finish

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,13 @@
 import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'node:fs/promises';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  try {
+    const contacts = await fs.readFile(PATH_DB, 'utf-8');
+    const data = JSON.parse(contacts);
+
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
